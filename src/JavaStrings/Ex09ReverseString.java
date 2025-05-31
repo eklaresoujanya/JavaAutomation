476fab12a6975aee825f336492271867c9a4bbb5
eklaresoujanya <eklaresoujanya@gmail.com>
2025-05-31 10:41:11 +0530

    31May String Reverse

---
+++ src/JavaStrings/Ex09ReverseString.java
@@ -0,0 +1,16 @@
+package JavaStrings;
+
+public class Ex09ReverseString {
+
+    public static void main(String[] args) {
+
+        String name = "Sowjanya";
+
+        for(int i=name.length()-1; i>=0;i--){
+
+            System.out.print(name.charAt(i));
+
+        }
+
+    }
+}
